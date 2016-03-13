e1dbc2a300ee439c7ef066031c1027d877a68a58
ZimonDai <daizhuoxian@gmail.com>
2016-03-13 01:17:19 -0800

    finish test1

--- tests/simple-duck/index.js
+++ tests/simple-duck/index.js
@@ -1,6 +1,13 @@
 var parser = new vox.Parser();
 
 var scene, camera, renderer, mesh;
+var updateListeners = [];
+var update = function(frame) {
+    updateListeners.forEach(function(listener) {
+        listener(frame);
+    });
+};
+var frame = 0;
 
 init();
 animate();
@@ -11,9 +18,26 @@ function init() {
 
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
   camera.position.z = 1000;
+  var cameraTarget = new THREE.Vector3(0, 0, 0);
+  updateListeners.push(function(frame) {
+      camera.position.x = Math.cos(frame * 0.004) * 100;
+      camera.position.y = 50;
+      camera.position.z = Math.sin(frame * 0.004) * 100;
+      camera.lookAt(cameraTarget);
+  });
 
+  var directionalLight = new THREE.DirectionalLight(0xffffff);
+  updateListeners.push(function(frame) {
+      directionalLight.position.x = Math.cos(frame * -0.001) * 100;
+      directionalLight.position.y = 100;
+      directionalLight.position.z = Math.sin(frame * -0.001) * 100;
+  });
+  scene.add(directionalLight);
 
-  var param = { voxelSize: 5 };
+  var ambientLight = new THREE.AmbientLight(0xaaaaaa);
+  scene.add(ambientLight);
+
+  var param = { voxelSize: 0.5 };
   parser.parse('../../models/duck_c.vox').then(function(voxelData) {
     var builder = new vox.MeshBuilder(voxelData, param);
     mesh = builder.createMesh();
@@ -28,11 +52,10 @@ function init() {
 }
 
 function animate() {
-
   requestAnimationFrame( animate );
   if (mesh) {
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
+    frame++;
+    update(frame++)
   }
 
   renderer.render( scene, camera );
